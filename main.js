b183110b3861ea74d0185519606106f1fe1ad943
Nathan Wilson <nswilson@gmail.com>
2018-01-09 12:04:40 -0500

    Remove debugging comments/console.logs

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ const FAMILY = 23.84;
 const SINGLE = 530.12;
 const DOUBLE = 2 * SINGLE;
 
+const slider = document.getElementById('slider-color');
+const familyInput = document.getElementById('family-input');
+const largeInput = document.getElementById('large-input');
+const mediumInput = document.getElementById('medium-input');
+const singleBtn = document.getElementById('single');
+const doubleBtn = document.getElementById('double');
+
 let batchSize = SINGLE;
 
 /* Parameters:
@@ -14,112 +21,88 @@ let batchSize = SINGLE;
  * Returns: an array of [family, large, medium]
  */
 function calculateDoughBalls(batch, handle0, handle1) {
-  medium = handle0 / MEDIUM;
-  large  = (handle1 - handle0) / LARGE;
-  family = (batch - handle1) / FAMILY;
+  const medium = handle0 / MEDIUM;
+  const large  = (handle1 - handle0) / LARGE;
+  const family = (batch - handle1) / FAMILY;
 
   return [family, large, medium];
 }
 
-/* Tests
-*/
-test1 = calculateDoughBalls(batchSize, 0, 225)
-
-
-const slider = document.getElementById('slider-color');
-const familyInput = document.getElementById('family-input');
-const largeInput = document.getElementById('large-input');
-const mediumInput = document.getElementById('medium-input');
-const singleBtn = document.getElementById('single');
-const doubleBtn = document.getElementById('double');
-
 noUiSlider.create(slider, {
-  start: [ 0, 267.8 ],
+  start: [0, 267.8],
   connect: [true, true, true],
   direction: 'rtl',
   behaviour: 'drag',
   range: {
-    'min': [  0 ],
-    'max': [ batchSize ]
-  }
+    min: [0],
+    max: [batchSize],
+  },
 });
 
-const connect = slider.querySelectorAll('.noUi-connect');
-const classes = ['c-1-color', 'c-2-color', 'c-3-color', 'c-4-color', 'c-5-color'];
-
-for ( let i = 0; i < connect.length; i++ ) {
-  connect[i].classList.add(classes[i]);
+{
+  const connect = slider.querySelectorAll('.noUi-connect');
+  connect[0].classList.add('c-1-color');
+  connect[1].classList.add('c-2-color');
+  connect[2].classList.add('c-3-color');
 }
 
-/* 'update' is called with two arguments:
- *  - values: an array of handle values
- *  - handle: an index corresponding to the value that changed
- *
- */
-slider.noUiSlider.on('update', function( values, handle ) {
-
-  // var value = values[handle];
+slider.noUiSlider.on('update', (values) => {
   const [family, large, medium] = calculateDoughBalls(batchSize, values[0], values[1]);
 
   familyInput.value = family.toFixed(1);
   largeInput.value = large.toFixed(1);
   mediumInput.value = medium.toFixed(1);
-
-  // console.log("update event: %s", values.toString());
-  // console.log("result: %s, %s, %s", family, large, medium);
-  // if ( handle ) {
-  // inputNumber.value = value;
-  // } else {
-  // select.value = Math.round(value);
-  // }
 });
 
 function familyInputChanged() {
   slider.noUiSlider.set([null, batchSize - (familyInput.value * FAMILY)]);
-  console.log("change %s", familyInput.value);
 }
+
 function largeInputChanged() {
-  // set handle0 to what is left from family and larges
-  families = familyInput.value * FAMILY;
-  larges = largeInput.value * LARGE;
+  const families = familyInput.value * FAMILY;
+  const larges = largeInput.value * LARGE;
   slider.noUiSlider.set([batchSize - (families + larges), null]);
-  console.log("change %s", largeInput.value);
 }
+
 function mediumInputChanged() {
-    slider.noUiSlider.set([mediumInput.value * MEDIUM, null]);
-    console.log("change %s", mediumInput.value);
+  slider.noUiSlider.set([mediumInput.value * MEDIUM, null]);
 }
 
 mediumInput.addEventListener('change', mediumInputChanged);
 largeInput.addEventListener('change', largeInputChanged);
 familyInput.addEventListener('change', familyInputChanged);
 
-document.getElementById('up-family').addEventListener('click', function() {
+document.getElementById('up-family').addEventListener('click', () => {
   familyInput.value = Math.floor(familyInput.value) + 1;
   familyInputChanged();
 });
-document.getElementById('down-family').addEventListener('click', function() {
+
+document.getElementById('down-family').addEventListener('click', () => {
   familyInput.value = Math.ceil(familyInput.value) - 1;
   familyInputChanged();
 });
-document.getElementById('up-large').addEventListener('click', function() {
+
+document.getElementById('up-large').addEventListener('click', () => {
   largeInput.value = Math.floor(largeInput.value) + 1;
   largeInputChanged();
 });
-document.getElementById('down-large').addEventListener('click', function() {
+
+document.getElementById('down-large').addEventListener('click', () => {
   largeInput.value = Math.ceil(largeInput.value) - 1;
   largeInputChanged();
 });
-document.getElementById('up-medium').addEventListener('click', function() {
+
+document.getElementById('up-medium').addEventListener('click', () => {
   mediumInput.value = Math.floor(mediumInput.value) + 1;
   mediumInputChanged();
 });
-document.getElementById('down-medium').addEventListener('click', function() {
+
+document.getElementById('down-medium').addEventListener('click', () => {
   mediumInput.value = Math.ceil(mediumInput.value) - 1;
   mediumInputChanged();
 });
 
-singleBtn.addEventListener('click', function(e) {
+singleBtn.addEventListener('click', (e) => {
   e.preventDefault();
   doubleBtn.classList.remove('active');
   singleBtn.classList.add('active');
@@ -128,10 +111,11 @@ singleBtn.addEventListener('click', function(e) {
     range: {
       min: 0,
       max: SINGLE,
-    }
+    },
   });
 });
-doubleBtn.addEventListener('click', function(e) {
+
+doubleBtn.addEventListener('click', (e) => {
   e.preventDefault();
   singleBtn.classList.remove('active');
   doubleBtn.classList.add('active');
@@ -140,7 +124,7 @@ doubleBtn.addEventListener('click', function(e) {
     range: {
       min: 0,
       max: DOUBLE,
-    }
+    },
   });
 });
 
